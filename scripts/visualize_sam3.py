81696e3d65a4d5e48ea7cd0b955c2fb62ecb0ad5
Elephant200 <alexktev@gmail.com>
2026-05-11 16:34:33 -0400

    Create visualization script

--- scripts/visualize_sam3.py
+++ scripts/visualize_sam3.py
@@ -1,10 +1,40 @@
 from datetime import date
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
 from elephant_id.ai import Sam3Service
 from elephant_id.dataset import Dataset
-from elephant_id.models import Photo
-from elephant_id.visualization import draw_rle_mask_overlay
+from elephant_id.visualize import visualize_predictions
 
 if __name__ == "__main__":
-    dataset = Dataset(dataset_root=Path("dataset/elephants-alive/coded"), metadata_path=Path("dataset/elephants-alive/images.csv"))
-    sighting = dataset.get_photo("Devin_2015-11-05_09.JPG")
+    load_dotenv()
+    
+    dataset = Dataset(
+        dataset_root=Path("dataset/elephants-alive/coded"),
+        metadata_path=Path("dataset/elephants-alive/images.csv"),
+        image_cache_size=32,
+    )
+    sighting = dataset.get_sighting("Devin", date(2015, 11, 5))
+    sam3 = Sam3Service(
+        api_key=os.getenv("ROBOFLOW_API_KEY"),
+        dataset=dataset,
+        cache_root=Path(".cache"),
+        workspace_name="seek-identification",
+        workflow_id="sam3",
+        confidence_threshold=0.6,
+        nms=True,
+        nms_iou_threshold=0.2,
+    )
+    print(sighting)
+    sample_photo = sighting.photos[0]
+    print(sample_photo)
+    original_image = dataset.read_image(sample_photo)
+    print("Running SAM3...")
+    predictions = sam3.run(sample_photo, "elephant")
+    print("Predictions:")
+    print(predictions)
+    visualized_image = visualize_predictions(original_image, predictions["predictions"])
+    visualized_image.show()
+    visualized_image.save("visualized_image.jpg")
