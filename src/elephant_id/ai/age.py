b367d3a0e75c1b2d312dd7118af785c66f55b1ba
Elephant200 <alexktev@gmail.com>
2026-05-18 21:36:55 -0400

    Add decode logic

--- src/elephant_id/ai/age.py
+++ src/elephant_id/ai/age.py
@@ -7,6 +7,7 @@ from elephant_id.cache import CacheManager
 from elephant_id.constants import DEFAULT_CACHE_ROOT
 from elephant_id.dataset import Dataset
 from elephant_id.domain import Photo
+from elephant_id.visualize import decode_rle_mask
 
 
 class AgeRunner:
@@ -39,12 +40,16 @@ class AgeService:
             cache_root=cache_root,
         )
 
-    def run(self, photo: Photo, body_mask: np.ndarray) -> dict:
+    def run(self, photo: Photo, body_rle_mask: np.ndarray) -> dict:
         key = f"{photo.identifier}"
+
+        body_mask = decode_rle_mask(body_rle_mask)
+        # TODO: crop the image to include only the body
+        crop_xyxy = (0, 0, body_mask.shape[1], body_mask.shape[0])
 
         return self.cache_manager.get_or_compute(
             key=key,
             compute_fn=lambda: self.runner.run(
-                image=self.dataset.read_image(photo, mask=body_mask),
+                image=self.dataset.read_image(photo, mask=body_mask, crop=crop_xyxy),
             ),
         )
