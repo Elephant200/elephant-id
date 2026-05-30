9923179155376da76f227a4312a8bf01c7d7b64e
Elephant200 <alexktev@gmail.com>
2026-05-30 15:03:36 -0400

    Fix name

--- src/elephant_id/coding/coder.py
+++ src/elephant_id/coding/coder.py
@@ -2,7 +2,7 @@
 Generates a SEEK code for one elephant
 """
 
-from elephant_id.coding.features import FeatureComputeService
+from elephant_id.coding.photo_analyzer import PhotoAnalyzer
 from elephant_id.dataset import Dataset
 from elephant_id.domain import SeekCode, Sighting
 
@@ -10,11 +10,10 @@ from elephant_id.domain import SeekCode, Sighting
 class SeekCoder:
     def __init__(self, dataset: Dataset) -> None:
         self.dataset = dataset
-        self.feature_compute_service = FeatureComputeService(dataset)
+        self.photo_analyzer = PhotoAnalyzer(dataset)
 
     def code(self, sighting: Sighting) -> SeekCode:
-        all_features = []
         for photo in sighting.photos:
-            all_features.append(self.feature_compute_service.compute(photo))
+            photo_analysis = self.photo_analyzer.analyze(photo)
 
-        # Use features to generate SEEK codes
+        # Use photo analysis to generate SEEK codes
