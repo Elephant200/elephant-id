1dfab02372b2212f7760d8c3cbe35e99307af72f
Elephant200 <alexktev@gmail.com>
2026-06-01 21:59:38 -0400

    Add placeholder code

--- src/elephant_id/coding/coder.py
+++ src/elephant_id/coding/coder.py
@@ -15,5 +15,26 @@ class SeekCoder:
     def code(self, sighting: Sighting) -> SeekCode:
         for photo in sighting.photos:
             photo_analysis = self.photo_analyzer.analyze(photo)
+            if photo_analysis is None:
+                continue
 
         # Use photo analysis to generate SEEK codes
+        return SeekCode(
+            g=...,
+            a=...,
+            tr=...,
+            tl=...,
+            ert1=...,
+            erh1=...,
+            ert2=...,
+            erh2=...,
+            elt1=...,
+            elh1=...,
+            elt2=...,
+            elh2=...,
+            xr=...,
+            xl=...,
+            sr=...,
+            sl=...,
+            sb=...,
+        )
