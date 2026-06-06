4af77103c87b5879c3f5badf0d10d456d406741d
Elephant200 <alexktev@gmail.com>
2026-06-05 21:13:18 -0400

    Test hole algorithms

--- scripts/holes.py
+++ scripts/holes.py
@@ -6,7 +6,7 @@ import numpy as np
 from elephant_id.ai import AnchorService, Sam3Service
 from elephant_id.coding.analyzers.ears import Ear
 from elephant_id.dataset import Dataset
-from elephant_id.image.transforms import apply_mask
+from elephant_id.image.transforms import apply_crop, apply_mask
 
 
 def find_log_contours(
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     sam3 = Sam3Service(dataset=dataset)
     anchor_model = AnchorService(dataset=dataset)
 
-    photo = dataset.get_photo("Bloom_2016-06-06_08")
+    photo = dataset.get_photo("Centaures_2018-11-24_10")
 
     detections = sam3.run(photo, "features")
     ear_detections = [detection for detection in detections if detection.class_name == "ear"]
@@ -56,64 +56,66 @@ if __name__ == "__main__":
             anchor_dets = sorted(anchor_dets, key=lambda d: d.confidence, reverse=True)[0]
         ears.append(Ear(ear_detection, anchor_dets[0]))
 
-    ear = max(ears, key=lambda e: e.area)
+    ear = min(ears, key=lambda e: e.area)
 
     # crop to ear
-    ear_only = apply_mask(dataset.read_image(photo), ear.get_mask(), crop=True)
+    image = apply_crop(dataset.read_image(photo), ear.xyxy)
+    ear_mask = ear.get_mask()[int(ear.xyxy[1]):int(ear.xyxy[3]), int(ear.xyxy[0]):int(ear.xyxy[2])].copy()
+    ear_only = apply_mask(image, ear_mask)
     cv2.imshow("Ear only", ear_only)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    ear_only_gray = 255 - cv2.cvtColor(ear_only, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Ear only gray (inverted)", ear_only_gray)
+    lab = cv2.cvtColor(ear_only, cv2.COLOR_BGR2LAB)
+    ear_grayscale, _, _ = cv2.split(lab)
+    cv2.imshow("Ear grayscale", ear_grayscale)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # Test different hole finding algorithms
 
-    # 1) Canny edge detection
+    # Preprocess by subtracting the background
 
-    # Run Canny edge detection, then find contours
-    blurred = cv2.GaussianBlur(ear_only, (3, 3), 0)
-    edges = cv2.Canny(blurred, 75, 200)
+    local_mean = cv2.GaussianBlur(ear_grayscale, (121, 121), 0)
+    # Delete area outside mask
+    local_mean[~ear_mask] = 0
+    cv2.imshow("Local mean", local_mean)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    normalized_grayscale = cv2.absdiff(ear_grayscale, local_mean)
+    cv2.imshow("Normalized grayscale", normalized_grayscale)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    edges = cv2.Canny(normalized_grayscale, 125, 300)
     cv2.imshow("Edges", edges)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
     contours, _ = cv2.findContours(edges, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-    ear_only_copy = ear_only.copy()
+    binary_copy = normalized_grayscale.copy()
+    original_copy = image.copy()
     for contour in contours:
-        cv2.drawContours(ear_only_copy, [contour], -1, (0, 255, 0), 2)
-    cv2.imshow("Contours", ear_only_copy)
+        cv2.drawContours(binary_copy, [contour], -1, (255, 255, 255), 1)
+        cv2.drawContours(original_copy, [contour], -1, (0, 255, 0), 1)
+    cv2.imshow("Contours", binary_copy)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # 2)  Run blob finding (pretty bad)
-    # params = cv2.SimpleBlobDetector_Params()
+    cv2.imwrite("edges.png", binary_copy)
+    cv2.imwrite("original.png", original_copy)
 
-    # params.filterByArea = True
-    # params.minArea = 10
+    # Filter for connected contours
 
-    # detector = cv2.SimpleBlobDetector_create(params)
-    # keypoints = detector.detect(ear_only_gray)
-    # print(len(keypoints))
-    # for keypoint in keypoints:
-    #     cv2.circle(ear_only, (int(keypoint.pt[0]), int(keypoint.pt[1])), int(keypoint.size), (0, 255, 0), 5)
-    # cv2.imshow("Blobs", ear_only)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
     # 3) Run Laplacian of Gaussian
-    # gaussian = cv2.GaussianBlur(ear_only_gray, (5, 5), 0)
-    # cv2.imshow("Gaussian", gaussian)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # laplacian = cv2.Laplacian(gaussian, cv2.CV_16S)
-    # cv2.imshow("Laplacian", cv2.convertScaleAbs(laplacian))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # contours = find_log_contours(ear_only_gray)
-    # for contour in contours:
-    #     cv2.drawContours(ear_only, [contour], -1, (0, 255, 0), 2)
-    # cv2.imshow("Contours", ear_only)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    laplacian = cv2.Laplacian(normalized_grayscale, cv2.CV_16S)
+    cv2.imshow("Laplacian", cv2.convertScaleAbs(laplacian))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    contours = find_log_contours(normalized_grayscale)
+    for contour in contours:
+        cv2.drawContours(ear_only, [contour], -1, (0, 255, 0), 2)
+    cv2.imshow("Contours", ear_only)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
