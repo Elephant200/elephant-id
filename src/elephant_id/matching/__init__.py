a4fcbfcbf8ad6c1ee738a3c7d261e8a15a31fb3c
Elephant200 <alexktev@gmail.com>
2026-06-26 18:57:23 -0400

    Export all types

--- src/elephant_id/matching/__init__.py
+++ src/elephant_id/matching/__init__.py
@@ -1,5 +1,17 @@
 """Tear-profile matching utilities."""
 
-from elephant_id.matching.tear_matcher import TearMatcher, TearMatcherConfig
+from elephant_id.matching.tear_matcher import (
+    TearMatch,
+    TearMatchBatch,
+    TearMatcher,
+    TearMatcherConfig,
+    TearMatchGallery,
+)
 
-__all__ = ["TearMatcher", "TearMatcherConfig"]
+__all__ = [
+    "TearMatch",
+    "TearMatchBatch",
+    "TearMatchGallery",
+    "TearMatcher",
+    "TearMatcherConfig",
+]
